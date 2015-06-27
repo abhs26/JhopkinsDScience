0cf08fbc74746c7d61073f430b220f3400421240
abhishek <abhs26@gmail.com>
2015-06-27 12:10:18 +0530

    graph.py upload

---
+++ graph.py
@@ -0,0 +1,44 @@
+class Vertex:
+    def __init__(self,key):
+        self.id = key  #vertex number
+        self.connectedTo = {}  #dictionary to maintain list of connected vertex
+
+
+    def addNeighbour(self,nbr,weight=0):
+        self.connectedTo[nbr]=weight
+
+
+    def __str__(self):
+        return str(self.id)+ 'connected to'+ str([x.id for x in self.connectedTo])
+
+
+    def getConnections(self):
+        return self.connectedTo.keys()
+
+    def getId(self):
+        return self.id
+
+    def getWeight(self,nbr):
+        return self.connectedTo[nbr]
+
+
+class Graph:
+    def __init__(self):
+        self.vertList = {}
+        self.numVertices = 0
+
+    def addVertex(self,key):
+        self.numVertices = self.numVertices+1
+        newVertex = Vertex(key)
+        self.vertList[key]=newVertex
+        return newVertex
+
+    def getVertex(self,n):
+        if n in self.vertList:
+            return self.vertList[n]
+        else:
+            return None
+        
+        
+
+        
